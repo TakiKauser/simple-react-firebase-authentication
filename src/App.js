2f37ed0618fb776c5aa23e2b5a3bc58ab75f95ad
TakiKauser <takikauserbadhon@gmail.com>
2021-10-06 23:49:52 +0600

    bootstrap-form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import { GoogleAuthProvider } from "firebase/auth";
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
 import initializeAuthentication from './Firebase/firebase.initialize';
 import { useState } from 'react';
 
@@ -41,41 +41,75 @@ function App() {
       })
   }
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+  }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  }
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
-    <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
-      <br /> <br />
-      <button onClick={handleGitHubSignIn}>GitHub Sign In</button>
-      <br /> <br />
-      {
-        user.name && <div>
-          <h2>Hellow, {user.name}</h2>
-          <h3>Your email is: {user.email}</h3>
-          <img src={user.photo} alt="" />
+    <div className="mx-auto w-50">
+      <h3 className="text-primary text-center">Register</h3>
+      <form onSubmit={handleRegistration}>
+        <div class="row mb-3">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3" />
+          </div>
         </div>
-      }
+        <div class="row mb-3">
+          <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+          <div class="col-sm-10">
+            <input onBlur={handlePasswordChange} type="password" class="form-control" id="inputPassword3" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-sm-10 offset-sm-2">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck1" />
+              <label class="form-check-label" for="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+      </form>
     </div>
   );
 }
 
 export default App;
-
-/* const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  }); */
+/*  {!user.name ?
+   <div>
+     <button onClick={handleGoogleSignIn}>Google Sign In</button>
+     <br />
+     <button onClick={handleGitHubSignIn}>GitHub Sign In</button>
+     <br />
+   </div> :
+   <button onClick={handleSignOut}>Sign Out</button>
+ }
+ {
+   user.name && <div>
+     <h2>Hellow, {user.name}</h2>
+     <h3>Your email is: {user.email}</h3>
+     <img src={user.photo} alt="" />
+   </div>
+ } */
