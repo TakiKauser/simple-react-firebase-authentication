6f5a6d3c139267a9991cf2c829db3c395ce4f3b7
TakiKauser <takikauserbadhon@gmail.com>
2021-10-06 21:46:51 +0600

    google-popup-sign-in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+// import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from 'react';
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        // console.log(loggedInUser);
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <br /> <br />
+      {
+        user.email && <div>
+          <h2>Hellow, {user.name}</h2>
+          <h3>Your email is: {user.email}</h3>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
 
 export default App;
+
+/* const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  }); */
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOZo-5LPFN1NVN088BrwCZZC8JRxnGnMc",
+  authDomain: "simple-firebase-authenti-6e1b7.firebaseapp.com",
+  projectId: "simple-firebase-authenti-6e1b7",
+  storageBucket: "simple-firebase-authenti-6e1b7.appspot.com",
+  messagingSenderId: "506949154325",
+  appId: "1:506949154325:web:8c8563a536b186f21ec01c"
+};
+
+export default firebaseConfig;
